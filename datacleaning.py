eddb97125220ed5f6b5ac0cc2407f29b636144c9
bnbmuniz <92181494+bnbmuniz@users.noreply.github.com>
2022-01-26 22:03:12 +0000

    quase todas funcoes

---
+++ datacleaning.py
@@ -0,0 +1,12 @@
+#price ={'Web Development Bootcamp':12.000, 'Web Development Part-Time':13.000, 'UX/UI Design Bootcamp':12.000,
+#'UX/UI Design Part-Time':13.000, 'Cyber Security Bootcamp':7.500, 'Cybersecurity Part-Time':7.500, 'Data Analytics Bootcamp':7.500,
+#'Data Analytics Part-Time':8.000}
+#comments2['Price']=[comments2['School']=='Ironhack'].map(price)
+"""
+def price(row):
+    if (row['School'] == 'Ironhack') & (row['Bootcamp'] == )
+
+comments2.to_sql('comments2', con=conn, if_exists='replace', chunksize=1000)
+
+"""
+print(23425434)
